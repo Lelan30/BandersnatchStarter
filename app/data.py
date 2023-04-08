6445b481c33e490504a1a6e1f7772542e8306159
Ducky-007 <c.duckett93@gmail.com>
2023-04-07 20:19:45 -0700

    updated database class and methods_3

--- app/data.py
+++ app/data.py
@@ -8,8 +8,8 @@ from pymongo import MongoClient
 
 
 class Database:
-    '''Database class to connect to database and use functions to add
-    documents or reset database as well as creating a dataframe'''
+    """Database class to connect to database and use functions to add
+    documents or reset database as well as creating a dataframe"""
     # connect to database
     load_dotenv()
     database = MongoClient(getenv('DB_URL'), tlsCAFile=where())['Database']
@@ -19,22 +19,23 @@ class Database:
         self.collection = self.database[collection]
 
     def seed(self, amount: int) -> str:
-        '''inserts the specified number of documents into the collection'''
+        """inserts the specified number of documents into the collection"""
         docs = [Monster().to_dict() for _ in range(0, amount)]
         self.collection.insert_many(docs)
-        print(f'{amount} documents have been added to Database')
+        print('Documents have been added to Database')
 
     def reset(self) -> str:
-        '''deletes all documents from the collection'''
+        """deletes all documents from the collection"""
         self.collection.delete_many({})
         print('Database has been reset!')
 
     def count(self) -> int:
-        '''returns the number of documents in the collection'''
-        return self.collection.count_documents({})
+        """returns the number of documents in the collection"""
+        total_docs = self.collection.count_documents({})
+        print(total_docs)
 
     def dataframe(self) -> DataFrame:
-        '''returns a DataFrame containing all documents in the collection'''
+        """returns a DataFrame containing all documents in the collection"""
         # create cursor to execute query
         cur = self.collection.find({})
         # create dataframe
@@ -42,15 +43,16 @@ class Database:
 
         # drop _id col
         data_frame = data_frame.drop('_id', axis=1)
-        print(f'DataFrame with {self.count()} documents has been created!')
-        return data_frame
+        print(data_frame)
 
     def html_table(self) -> str:
-        '''returns an HTML table representation
-        of the DataFrame, or None if the collection is empty'''
-        return self.dataframe().to_html()
+        """returns an HTML table representation
+        of the DataFrame, or None if the collection is empty"""
+        print(DataFrame(self.dataframe()).to_html())
 
 
 if __name__ == '__main__':
     # creating collection
     db = Database('Bandersnatch')
+    db.html_table()
+
