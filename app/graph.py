66152789a9cfe29b8df302e58b396fa717af4398
Nate <119789611+Lelan30@users.noreply.github.com>
2023-06-11 11:59:24 -0500

    Update graph.py

--- app/graph.py
+++ app/graph.py
@@ -6,8 +6,8 @@ def chart(df: DataFrame, x: str, y:str, target: str) -> Chart:
     Function: Create a chart object from the dataframe.
     """
     data = df[[x, y, target]]
-    title = f"y by {x} for {target}"
-    chart_objc = Chart(data).mark_circle(size=100).encode(
+    chart_objc = Chart(data, title = f"y by {x} for {target}")
+    .mark_circle(size=100).encode(
         x=x,
         y=y,
         color=target,
@@ -15,7 +15,7 @@ def chart(df: DataFrame, x: str, y:str, target: str) -> Chart:
     ).properties(
         width=450,
         height=450,
-        background='black',
+        background='white',
         padding=20,
     ).configure_axis(
         titleFontSize=20,
